77ece554ab25700c7e23820b59d021ee42357bc4
examine-dev <examineconsultoriaeducacional@gmail.com>
2025-01-16 09:31:55 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,14 +1,40 @@
-// Get the video element
-const videoElement = document.getElementById('videoElement');// Define media constraints
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById('snap');
+const erroMsgElement = document.querySelector('span#errorMsg');
+
 const constraints = {
-    video: true,
-    audio: true
-};// Request user media
-navigator.mediaDevices.getUserMedia(constraints)
-    .then(stream => {
-        // Attach the media stream to the video element
-        videoElement.srcObject = stream;
-    })
-    .catch(error => {
-        console.error('Error accessing media devices.', error);
-    });
+    video: {
+      with:640, height:480
+    },
+    audio: false
+};
+
+// Access webcan
+async function init() {
+  try {
+    alert('Init');
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSucess(stream);
+
+  } catch (error) {
+    erroMsgElement.innerHTML = `navigator.mediaDevices.getUserMedia error: ${error.toString()}`;
+  }
+  
+}
+
+function handleSucess(stream){
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// inicialize webcam acces function
+init();
+
+// draw image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function(){
+  context.drawImage(video, 0, 0,  640, 480);
+});   
+
